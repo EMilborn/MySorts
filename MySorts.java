93b6e03525e079d67f4453147187750a3d5b9dd1
EMilborn <emilborn@stuy.edu>
2015-12-29 13:57:41 -0500

    Update MySorts.java
    added tests in main, bogoSort works

--- MySorts.java
+++ MySorts.java
@@ -135,7 +135,40 @@ public class mySorts {
 
     //main method for testing
     public static void main( String [] args ) {
+	ArrayLis glen = new ArrayList<Integer>();
+	glen.add(7);
+	glen.add(1);
+	glen.add(5);
+	glen.add(12);
+	glen.add(3);
+	System.out.println( "ArrayList glen before sorting:\n" + glen );
+	bogoSortV(glen);
+	System.out.println( "ArrayList glen after sorting:\n" + glen );
 
+	ArrayList coco = populate( 10, 1, 1000 );
+	System.out.println( "ArrayList coco before sorting:\n" + coco );
+	bogoSortV(coco);
+	System.out.println( "ArrayList coco after sorting:\n" + coco );
+
+
+    	glen = new ArrayList<Integer>();
+	glen.add(7);
+	glen.add(1);
+	glen.add(5);
+	glen.add(12);
+	glen.add(3);
+	System.out.println( "ArrayList glen before sorting:\n" + glen );
+	ArrayList glenSorted = bogoSort( glen );
+	System.out.println( "sorted version of ArrayList glen:\n" 
+			    + glenSorted );
+	System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      	coco = populate( 10, 1, 1000 );
+	System.out.println( "ArrayList coco before sorting:\n" + coco );
+	ArrayList cocoSorted = bogoSort( coco );
+	System.out.println( "sorted version of ArrayList coco:\n" 
+			    + cocoSorted );
+	System.out.println( "ArrayList coco after sorting:\n" + coco );
     }//end main
     
 }//end class SelectionSort
